d4ae3fa0d9fffcdfb440d08116c1f4a081bdbe76
Epifanio Suárez Martínez <epifanio.suarez@owaristudios.com>
2021-03-21 23:21:54 +0100

    The options for expenses are incorporated

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# contabimo
+# Contabimo
+
+La aplicación de contabimo tiene como objetivo servir de forma sencilla a pequeñas empresas y/o autonomos para facturar, gestionar presupuestos y gastos.
+
+Para soluciones más complejas, deben valorar otras opciones.
+
+## Roapmap
+
+* [] Realizar una analitica de los datos
+* [] Gestionar los cambios desde la aplicación
+* [] Permitir los cambios de multiples autonomos y/o empresas
+* [] Exportación de movimientos para asesores
+
+## Personalización
+
+* Puedes cambiar el logo cambiando la imágen de /static/logo.jpg
+* Tus datos de empresa/autonomo puedes cambiarlos en my_data.json
+
+## Tecnologías
+
+Aplicación construida sobre Python + Flask + PonyORM + SQLite
--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import models
 app = Flask(__name__);
 
 app.config["DEBUG"] = True;
-app.config["SECRET_KEY"] = "fdfgdfgdfgdp90349g03j49jg3049jg0394tj094u2094u2039i0239ir0";
+app.config["SECRET_KEY"] = "R7Dmcd3cMVET1lk121J8sQLFJGgXRFkCDwYYAC0ESHSGzWzaaNpDF5MGLpzy";
 
 data = json.load(open("my_data.json", encoding="utf-8"));
 
@@ -199,19 +199,16 @@ def add_income():
 def copy_income(id):
     with models.orm.db_session:
         if models.orm.count(income for income in models.IncomeInvoice if income.id == id) > 0:
-            if request.method == "POST":
-                pass;
-            else:
-                last_identifier = list(models.orm.select(income.identifier for income in models.IncomeInvoice if income.date > datetime.datetime(int(time.strftime("%Y")), 1, 1)))[-1];
-                number = int(last_identifier[-3:]) + 1;
-                number = ("0" * (3 - len(str(number))))  + str(number);
+            last_identifier = list(models.orm.select(income.identifier for income in models.IncomeInvoice if income.date > datetime.datetime(int(time.strftime("%Y")), 1, 1)))[-1];
+            number = int(last_identifier[-3:]) + 1;
+            number = ("0" * (3 - len(str(number))))  + str(number);
 
-                identifier = data["DEFAULTS"]["START_IDENTIFIER"] + time.strftime("%Y") + number;
+            identifier = data["DEFAULTS"]["START_IDENTIFIER"] + time.strftime("%Y") + number;
 
-                income = models.IncomeInvoice[id];
-                companies = list(models.orm.select((company.id, company.name) for company in models.Companies).order_by(lambda: company.name));
+            income = models.IncomeInvoice[id];
+            companies = list(models.orm.select((company.id, company.name) for company in models.Companies).order_by(lambda: company.name));
 
-                return render_template("copy_income.html", identifier=identifier, income=income, companies=companies);
+            return render_template("copy_income.html", identifier=identifier, income=income, companies=companies);
         else:
             flash("No se encuentra el ingreso que intentas copiar.")
             return redirect(url_for("incomes"));
@@ -308,8 +305,134 @@ def edit_invoice(id):
             flash("No se encuentra el ingreso que intentas acceder.")
             return redirect(url_for("incomes"));
 
-# Products
+# Expenses
 
-@app.route("/products/delete/<id>/<invoice_id>")
-def delete_products(id, invoice_id):
-    pass;
+@app.route("/expenses", defaults={"page": 1})
+@app.route("/expenses/<int:page>")
+def expenses(page):
+    limit = 25;
+
+    with models.orm.db_session:
+        expenses = models.orm.select(expense for expense in models.ExpenseInvoice).order_by(lambda: expense.id);
+
+        maxpage = math.ceil(expenses.count() / limit);
+        expenses = list(expenses.page(page, limit));
+
+        return render_template("expenses.html", expenses=expenses, maxpage=maxpage, page=page);
+
+@app.route("/expenses/add", methods=["GET", "POST"])
+def add_expense():
+    if request.method == "POST":
+        with models.orm.db_session:
+            if models.orm.count(expense for expense in models.ExpenseInvoice if expense.identifier == request.form["identifier"]) > 0:
+                flash(f"El identificador {request.form['identifier']} está repetido, no se ha podido agregar el gasto.");
+                return redirect(url_for("expenses"));
+            else:
+                new_expense = models.ExpenseInvoice(
+                    company=request.form["company"],
+                    identifier=request.form["identifier"],
+                    date=datetime.datetime.now(),
+                    modification_date=datetime.datetime.now(),
+                    expiration_date=datetime.datetime.strptime(request.form["expiration_date"], "%d/%m/%Y"),
+                    status=request.form["status"],
+                    irpf=request.form["irpf"],
+                    iva=request.form["iva"],
+                    irpf_money=request.form["irpf_money"],
+                    iva_money=request.form["iva_money"],
+                    total=request.form["total"],
+                    notes=request.form["notes"]
+                );
+                models.orm.commit();
+
+                keys = list(dict(request.form).keys());
+                for field in range(10, len(keys), 3):
+                    position = keys[field][-3:];
+
+                    new_product = models.ExpenseProducts(
+                        invoice=new_expense.id,
+                        description=request.form["description" + position],
+                        amount=request.form["amount" + position],
+                        price=request.form["price" + position]
+                    );
+
+                flash("Se ha agregado correctamente el ingreso");
+        return redirect(url_for("expenses"));
+    else:
+        with models.orm.db_session:
+            companies = list(models.orm.select((company.id, company.name) for company in models.Companies).order_by(lambda: company.name));
+
+        return render_template("add_expense.html", companies=companies, iva=data["DEFAULTS"]["IVA"], irpf=data["DEFAULTS"]["IRPF"]);
+
+@app.route("/expenses/copy/<id>")
+def copy_expense(id):
+    with models.orm.db_session:
+        if models.orm.count(expense for expense in models.ExpenseInvoice if expense.id == id) > 0:
+            expense = models.ExpenseInvoice[id];
+            companies = list(models.orm.select((company.id, company.name) for company in models.Companies).order_by(lambda: company.name));
+
+            return render_template("copy_expense.html", expense=expense, companies=companies);
+        else:
+            flash("No se encuentra el ingreso que intentas copiar.")
+            return redirect(url_for("expenses"));
+
+@app.route("/expenses/delete/<id>")
+def delete_expense(id):
+    with models.orm.db_session:
+        if models.orm.count(expense for expense in models.ExpenseInvoice if expense.id == id) > 0:
+            models.ExpenseInvoice[id].delete();
+            flash("Se ha eliminado el gasto.")
+            return redirect(url_for("expenses"));
+        else:
+            flash("No se encuentra el gasto que intentas acceder.")
+            return redirect(url_for("expenses"));
+
+@app.route("/expenses/view/<id>")
+def view_expense(id):
+    with models.orm.db_session:
+        if models.orm.count(expense for expense in models.ExpenseInvoice if expense.id == id) > 0:
+            expense = models.ExpenseInvoice[id];
+            return render_template("view_expense.html", expense=expense);
+        else:
+            flash("No se encuentra el gasto que intentas acceder.")
+            return redirect(url_for("expenses"));
+
+@app.route("/expenses/edit/<id>", methods=["GET", "POST"])
+def edit_expense(id):
+    with models.orm.db_session:
+        if models.orm.count(expense for expense in models.ExpenseInvoice if expense.id == id) > 0:
+            if request.method == "POST":
+                expense = models.ExpenseInvoice[id];
+
+                expense.company = request.form["company"];
+                expense.modification_date = datetime.datetime.now();
+                expense.expiration_date = datetime.datetime.strptime(request.form["expiration_date"], "%d/%m/%Y");
+                expense.status = request.form["status"];
+                expense.irpf = request.form["irpf"];
+                expense.iva = request.form["iva"];
+                expense.irpf_money = request.form["irpf_money"];
+                expense.iva_money = request.form["iva_money"];
+                expense.total = request.form["total"];
+                expense.notes = request.form["notes"];
+
+                models.orm.select(expense_product for expense_product in models.ExpenseProducts if expense_product.invoice.id == expense.id).delete();
+
+                keys = list(dict(request.form).keys());
+                for field in range(10, len(keys), 3):
+                    position = keys[field][-3:];
+
+                    new_product = models.ExpenseProducts(
+                        invoice=expense.id,
+                        description=request.form["description" + position],
+                        amount=request.form["amount" + position],
+                        price=request.form["price" + position]
+                    );
+
+                return redirect(url_for("expenses"));
+            else:
+                expense = models.ExpenseInvoice[id];
+                companies = list(models.orm.select((company.id, company.name) for company in models.Companies).order_by(lambda: company.name));
+
+                return render_template("edit_expense.html", expense=expense, companies=companies);
+        else:
+            flash("No se encuentra el gasto que intentas acceder.")
+            return redirect(url_for("expenses"));
